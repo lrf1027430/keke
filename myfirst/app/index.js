56a2de5214b9b2175c2876373aac79028423ed74
chandlerliu <5401610@qq.com>
2017-02-03 21:37:24 +0800

    fdsfdsfds

--- myfirst/app/index.js
+++ myfirst/app/index.js
@@ -1,25 +1,25 @@
 //CommonJs
-// var sub = require('./sub');
-// require('./css/main.scss');
-// var $ = require('jquery');
-// var moment = require('moment');
-// var app  = document.createElement('div');
-// app.innerHTML = '<h1>Hello World it</h1>';
-// document.body.appendChild(app);
-// app.appendChild(sub());
-// $('body').append('<p>look at me! now is ' + moment().format() + '</p>');
-
-//ES6 模式 关闭或者打开
-import './css/main.scss';
-import generateText from './sub';
-import $ from 'jquery';
-import moment from 'moment';
-
-let app  = document.createElement('div');
-const myPromise = Promise.resolve(42);
-myPromise.then((number) => {
-    $('body').append('<p>aaaapromise result is ' + number + ' now is ' + moment().format() + '</p>');
-});
+var sub = require('./sub');
+require('./css/main.scss');
+var $ = require('jquery');
+var moment = require('moment');
+var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World it</h1>';
 document.body.appendChild(app);
-app.appendChild(generateText());
+app.appendChild(sub());
+$('body').append('<p>look at me! now is ' + moment().format() + '</p>');
+
+//ES6 模式 关闭或者打开
+// import './css/main.scss';
+// import generateText from './sub';
+// import $ from 'jquery';
+// import moment from 'moment';
+//
+// let app  = document.createElement('div');
+// const myPromise = Promise.resolve(42);
+// myPromise.then((number) => {
+//     $('body').append('<p>aaaapromise result is ' + number + ' now is ' + moment().format() + '</p>');
+// });
+// app.innerHTML = '<h1>Hello World it</h1>';
+// document.body.appendChild(app);
+// app.appendChild(generateText());
--- myfirst/app/sub.js
+++ myfirst/app/sub.js
@@ -1,13 +1,14 @@
-// function generateText() {
-//     var element = document.createElement('h2');
-//     element.innerHTML = "Hello h2 worldgfdgdgfd";
-//     return element;
-// }
-//
-// module.exports = generateText;
-
-export default function() {
+function generateText() {
     var element = document.createElement('h2');
-    element.innerHTML = "Hello h2 world hahaha";
+    element.innerHTML = "Hello h2 worldgfdgdgfd";
     return element;
 }
+
+module.exports = generateText;
+
+//ES6 模式 关闭或者打开
+// export default function() {
+//     var element = document.createElement('h2');
+//     element.innerHTML = "Hello h2 world hahaha";
+//     return element;
+// }
